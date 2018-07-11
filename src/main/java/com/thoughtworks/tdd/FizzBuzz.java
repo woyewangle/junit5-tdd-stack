047ea14fe2a022096b59cedde317a8239c6f056b
louis <louis.z.j.li@oocl.com>
2018-07-11 11:57:28 +0800

    [louis] complete ttd test

---
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.tdd;
+
+/**
+ * Created by twer on 3/28/14.
+ */
+public class FizzBuzz {
+
+
+    public String fizzBuzz(int number) {
+        StringBuffer temp = new StringBuffer();
+
+        if(number%3==0){
+            temp.append("Fizz");
+        }
+        if(number%5==0){
+            temp.append("Buzz");
+        }
+        if(number%7==0){
+            temp.append("Whizz");
+        }
+
+        return "".equals(temp.toString()) ? String.valueOf(number) : String.valueOf(temp);
+    }
+
+}
---
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -0,0 +1,135 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class FizzBuzzTest {
+
+    @Test
+    public  void  should_return_1_when_given_input_is_1(){
+        //given
+        int number=1;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("1"));
+    }
+    @Test
+    public  void  should_return_Fizz_when_given_input_is_3(){
+        //given
+        int number=3;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("Fizz"));
+    }
+
+    @Test
+    public  void  should_return__when_given_input_is_3(){
+        //given
+        int number=3;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("Fizz"));
+    }
+
+    @Test
+    public  void  should_return_Fizz_when_given_input_is_6(){
+        //given
+        int number=6;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("Fizz"));
+    }
+
+    @Test
+    public  void  should_return_Buzz_when_given_input_is_5(){
+        //given
+        int number=5;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("Buzz"));
+    }
+    @Test
+    public  void  should_return_FizzBuzz_when_given_input_is_15(){
+        //given
+        int number=15;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("FizzBuzz"));
+    }
+
+    @Test
+    public  void  should_return_Whizz_when_given_input_is_7(){
+        //given
+        int number=7;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("Whizz"));
+    }
+    @Test
+    public  void  should_return_FizzWhizz_when_given_input_is_21(){
+        //given
+        int number=21;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("FizzWhizz"));
+    }
+
+    @Test
+    public  void  should_return_BuzzWhizz_when_given_input_is_35(){
+        //given
+        int number=35;
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("BuzzWhizz"));
+    }
+
+    @Test
+    public  void  should_return_FizzBuzzWhizz_when_given_input_is_105(){
+        //given
+        int number=105;
+
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+        String actual =fizzBuzz.fizzBuzz(number);
+
+        //then
+        assertThat(actual, is("FizzBuzzWhizz"));
+    }
+}
+
+
